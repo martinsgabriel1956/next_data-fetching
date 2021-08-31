954a48ae7ac8082e3fc278c169f0b7e84fd19a33
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-08-31 17:27:02 -0300

    feat: using filesystem and path node modules inside the getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,26 @@
+import fs from "fs/promises";
+import path from "path";
+
 export default function Home(props) {
   const { products } = props;
 
   return (
     <ul>
-      {products.map(product => (
-        <li key={product.id}>
-          {product.title}
-        </li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
       ))}
     </ul>
-  )
+  );
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [
-        { id: 'p1', title: 'Product 1' },
-        { id: 'p2', title: 'Product 2' },
-      ]
-    }
-  }
+      products: data.products,
+    },
+  };
 }
