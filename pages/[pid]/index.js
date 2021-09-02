9e80b246c2ad8a641e554e9637953de71682d55a
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-02 10:44:06 -0300

    feat: loading paths dynamically

--- pages/[pid]/index.js
+++ pages/[pid]/index.js
@@ -5,8 +5,6 @@ import React from 'react';
 export default function ProductDetail(props) {
   const { loadedProduct } = props;
 
-  // if(!loadedProduct) return <p>Loading...</p>
-  
   return (
     <>
       <h1>{loadedProduct.title}</h1>
@@ -15,14 +13,20 @@ export default function ProductDetail(props) {
   )
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
 
   const projectId = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find(product => product.id === projectId);
 
@@ -34,10 +38,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map(product => product.id);
+
+  const pathsWithParams = ids.map(id => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-     ],
-    fallback: 'blocking'
+    paths: pathsWithParams,
+    fallback: false
   }
 }
