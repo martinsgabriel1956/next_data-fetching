2e7c3c989e19802ab1fd86c99742c77378ed4cbf
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-02 14:08:01 -0300

    feat: fallback pages & not found pages

--- pages/[pid]/index.js
+++ pages/[pid]/index.js
@@ -30,6 +30,7 @@ export async function getStaticProps(context) {
 
   const product = data.products.find(product => product.id === projectId);
 
+  if(!product) return { notFound: true };
   return {
     props: {
       loadedProduct: product
